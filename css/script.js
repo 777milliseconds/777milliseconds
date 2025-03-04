d8f2a29a9e2235aba9b2540c8fbb8f311ac6a3f2
unknown <pasir7gg@gmail.com>
2025-03-05 03:56:27 +0700

    update

---
+++ css/script.js
@@ -0,0 +1,61 @@
+let name = 'yvlpasir'.split('');
+let nameElement = document.querySelector('.name');
+for(let i = 1; i < name.length+1; i++) {
+    setTimeout(function () {
+        nameElement.textContent += name[i-1];
+    }, 500 * i);
+}
+
+let played = false;
+var audio = new Audio('MOJO JOJO.mp3');
+let icon = document.querySelector('.fa-play-circle');
+audio.play()
+played = true;
+icon.classList.remove('fa-play-circle');
+icon.classList.add('fa-pause-circle');
+
+function play() {
+    if(played) {
+        audio.pause()
+        played = false;
+        icon.classList.remove('fa-pause-circle');
+        icon.classList.add('fa-play-circle');
+    } else {
+        audio.play()
+        played = true;
+        icon.classList.remove('fa-play-circle');
+        icon.classList.add('fa-pause-circle');
+    }
+}
+
+function test() {
+    let inputBox = document.querySelector(".input-box");
+    let inputText = inputBox.textContent.trim();
+
+    if (inputText !== "") {
+        fetch("https://deadpan-blushing-steam.glitch.me/post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: inputText })
+        })
+        .then((res) => {
+            if(res.status === 200){
+                window.location.href = "https://deadpan-blushing-steam.glitch.me/sent";
+            } else {
+                window.location.href = "https://deadpan-blushing-steam.glitch.me/error";
+            }
+        });
+        inputBox.textContent = "";
+        inputBox.focus();
+        document.execCommand("insertText", false, "");
+    }
+}
+
+document.querySelector(".input-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        test();
+    }
+});
